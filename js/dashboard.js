c2efbddc6b29ce300c6af9f706e7b4caaf27fdad
Mark <marknino175@gmail.com>
2025-12-26 16:18:11 +0800

    feat: add updateDashboard() to update dashboard UI with latest financial data

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,2 +1,12 @@
+// UPDATE ENTIRE DASHBOARD
 
+function updateDashboard() {
+  const hasData = financeData.hasData;
 
+  Utils.toggleEmptyState("empty-state", "dashboard-content", hasData);
+  // UPDATE SUMMARY CARDS
+  Utils.updateField("totalIncome", financeData.totalIncome);
+  Utils.updateField("totalExpenses", financeData.totalExpenses);
+  Utils.updateField("balance", financeData.balance);
+  Utils.updateField("budgetUsedPercentage", financeData.budgetUsedPercentage);
+}
